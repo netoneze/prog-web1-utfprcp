d3c19e56c0a502d8cd285de741d00f4833bb780c
NELSON ANTONIO NETO TONEZE <netoneze@hotmail.com>
2021-08-10 16:46:11 -0300

    Corrigido src das imagens e feito imports necessários

--- eaduvida/src/view/home/index.js
+++ eaduvida/src/view/home/index.js
@@ -1,5 +1,10 @@
 import React from 'react';
 import NavBar from '../../components/navbar';
+import LogoClassRoom from '../../img/google-classroom-logo-1.png';
+import LogoMeet from '../../img/google-meet-logo-1.png';
+import LogoJamboard from '../../img/jamboard-logo-1.png';
+import LogoKahoot from '../../img/kahoot-logo-1.png';
+
 function Home(){
     return (
         <>
@@ -12,23 +17,23 @@ function Home(){
                     <div className="plataformas-principal">
                         <div className="plataformas-principal-classroom">
                             <p className="texto-plataformas-principal">
-                                <img src="../../img/google-classroom-logo-1.png" className="img-plataformas-principal"
+                                <img src={LogoClassRoom} className="img-plataformas-principal"
                                      alt="Google Classroom" title="Logotipo Google Classroom"/>
                                     Google Classroom é um sistema de gerenciamento de conteúdo para escolas que procuram
                                     simplificar a
                                     criação, a distribuição e a avaliação de trabalhos.
                             </p>
                         </div>
-                        <div className="plataformas-principal-classroom">
+                        <div className="plataformas-principal-meet">
                             <p className="texto-plataformas-principal">
-                                <img src="../../img/google-meet-logo-1.png" className="img-plataformas-principal"
+                                <img src={LogoMeet} className="img-plataformas-principal"
                                      alt="Google Meet" title="Logotipo Google Meet"/>
                                     Google Meet é um serviço de comunicação por vídeo desenvolvido pelo Google.
                             </p>
                         </div>
                         <div className="plataformas-principal-jamboard">
                             <p className="texto-plataformas-principal">
-                                <img src="../../img/jamboard-logo-1.png" className="img-plataformas-principal" alt="Jamboard"
+                                <img src={LogoJamboard} className="img-plataformas-principal" alt="Jamboard"
                                      title="Logotipo Jamboard"/>
                                     O Jamboard é um quadro interativo desenvolvido pelo Google, como parte da família G
                                     Suite.
@@ -36,7 +41,7 @@ function Home(){
                         </div>
                         <div className="plataformas-principal-kahoot">
                             <p className="texto-plataformas-principal">
-                                <img src="../../img/kahoot-logo-1.png" className="img-plataformas-principal" alt="Kahoot"
+                                <img src={LogoKahoot} className="img-plataformas-principal" alt="Kahoot"
                                      title="Logotipo Kahoot"/>
                                     Kahoot! é uma plataforma de aprendizado baseada em jogos, usada como tecnologia
                                     educacional em escolas e outras instituições de ensino.
