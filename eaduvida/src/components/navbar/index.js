9d0d72e71fd425833f140c7c0e7ae53a5dc3f54d
NELSON ANTONIO NETO TONEZE <netoneze@hotmail.com>
2021-08-19 10:50:53 -0300

    Modificado menu para ficar dinâmico de acordo com o login do usuário

--- eaduvida/src/components/navbar/index.js
+++ eaduvida/src/components/navbar/index.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import {Link} from 'react-router-dom';
 import Logo from '../../img/logo.png';
-import {useSelector} from "react-redux";
+import {useDispatch, useSelector} from "react-redux";
 
 function NavBar(){
+    const dispatch = useDispatch();
+
     return (
         <header className="item-container">
             <div className="caixa-superior">
@@ -11,11 +13,34 @@ function NavBar(){
                      alt="Logotipo EADúvida" title="Logotipo superior EADúvida"/>
                     <nav>
                         <ul>
-                            <li><Link to="/" className="nav-atual">Início</Link></li>
-                            <li><Link to="/login">Login</Link></li>
-                            <li><Link to="/tutorial">Tutoriais</Link></li>
-                            <li><Link to="/contato">Contato</Link></li>
-                            <li><Link to="/sobre">Sobre</Link></li>
+                            {
+                                useSelector(state => state.usuarioLogado) > 0 ?
+                                    <>
+                                        <li><Link to="/" className="nav-atual">Início</Link></li>
+                                        <li className="nav-item dropdown">
+                                            <a className="nav-link dropdown-toggle" href="#" id="navbarDropdown"
+                                               role="button" data-toggle="dropdown" aria-haspopup="true"
+                                               aria-expanded="false">
+                                                Meu Perfil
+                                            </a>
+                                            <div className="dropdown-menu" aria-labelledby="navbarDropdown">
+                                                <Link className="dropdown-item" to="/perfil">Perfil</Link>
+                                                <Link className="dropdown-item" onClick={() => dispatch({type: 'LOGOUT'})}>Sair</Link>
+                                            </div>
+                                        </li>
+                                        <li><Link to="/tutorial">Tutoriais</Link></li>
+                                        <li><Link to="/contato">Contato</Link></li>
+                                        <li><Link to="/sobre">Sobre</Link></li>
+                                    </>
+                                    :
+                                    <>
+                                        <li><Link to="/" className="nav-atual">Início</Link></li>
+                                        <li><Link to="/login">Login</Link></li>
+                                        <li><Link to="/tutorial">Tutoriais</Link></li>
+                                        <li><Link to="/contato">Contato</Link></li>
+                                        <li><Link to="/sobre">Sobre</Link></li>
+                                    </>
+                            }
                         </ul>
                     </nav>
             </div>
