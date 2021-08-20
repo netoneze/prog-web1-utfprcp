3be20f5f95394c340d4638819f6609b4406b76c9
NELSON ANTONIO NETO TONEZE <netoneze@hotmail.com>
2021-08-20 16:11:27 -0300

    Adicionado campo de nome e centralizado onClick em apenas um button

--- eaduvida/src/view/perfil/index.js
+++ eaduvida/src/view/perfil/index.js
@@ -9,18 +9,27 @@ import $ from 'jquery';
 function Perfil(){
 
     const [imagem, setImagem] = useState();
+    const [nomeUsuario, setNomeUsuario] = useState();
 
     const storage = firebase.storage();
     const db = firebase.firestore();
+    const emailUsuario = useSelector(state => state.usuarioEmail)
 
-    function cadastrarImagemUsuario(){
+    function salvar(){
         storage.ref(`imagens/${imagem.name}`).put(imagem).then(() => {
-            alert("Imagem salva com sucesso!");
-            $('#input-form')[0].reset();
-        }).catch(erro => {
-            alert(erro);
-        })
+            db.collection('usuarios').add({
+                nome: nomeUsuario,
+                usuarioEmail: emailUsuario,
+                imagem: imagem.name
+            }).then(() => {
+                alert("Modificações salvas com sucesso!");
+                return [$('#imagem-usuario')[0].reset(), $('#nome-usuario').val('')]
+            }).catch(erro => {
+                alert(erro);
+            })
+        });
     }
+
     return (
         <>
             {
@@ -30,16 +39,25 @@ function Perfil(){
             <main className="item-container" id="itemPrincipal">
                 <div className="principal">
                     <h2 className="titulo-principal">Perfil</h2>
-                    <div className="form-group">
-                        <div className="text-center">
-                            <label className="help-text">Imagem de usuário</label>
+                    <form>
+                        <div className="form-group">
+                            <div className="text-center">
+                                <label className="help-text">Imagem de usuário</label>
+                            </div>
+                            <img src={User} className="img-usuario-perfil img-thumbnail rounded mx-auto d-block" alt="Imagem de usuário"/>
                         </div>
-                        <img src={User} className="img-usuario-perfil img-thumbnail rounded mx-auto d-block" alt="Imagem de usuário"/>
-                    </div>
-                    <form id="input-form" className="form-group">
-                        <label>Upload de imagem:</label>
-                        <input type="file" className="form-control" onChange={(e) => setImagem(e.target.files[0])}/>
-                        <button onClick={cadastrarImagemUsuario} type="button" className="btn btn-primary my-2" id="btnEntrar">Fazer upload</button>
+                        <form id="imagem-usuario" className="form-group">
+                            <label>Upload de imagem:</label>
+                            <input type="file" className="form-control" onChange={(e) => setImagem(e.target.files[0])}/>
+                        </form>
+                        <div className="form-group">
+                            <label htmlFor="InputUsuario">Nome de usuário</label>
+                            <input onChange={(e) => setNomeUsuario(e.target.value)} name="nome" type="text" className="form-control" id="nome-usuario"
+                                   placeholder="Digite um nome de usuário" required/>
+                        </div>
+                        <div className="text-center">
+                            <button type="button" onClick={salvar} className="btn btn-lg btn-primary my-2" >Salvar</button>
+                        </div>
                     </form>
                     <span><p className="help-text">Personalize seu perfil de usuário</p></span>
                 </div>
