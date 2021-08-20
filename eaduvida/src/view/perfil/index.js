dabfd8c71f0c2d52a9d7b17f87d2d92a898585ef
NELSON ANTONIO NETO TONEZE <netoneze@hotmail.com>
2021-08-20 11:12:52 -0300

    Adicionado método para limpar input após enviar imagem de usuário

--- eaduvida/src/view/perfil/index.js
+++ eaduvida/src/view/perfil/index.js
@@ -3,6 +3,9 @@ import User from '../../img/user.png'
 import firebase from "firebase";
 import NavBar from "../../components/navbar";
 import Footer from "../../components/footer";
+import {useSelector} from "react-redux";
+import {Redirect} from "react-router-dom";
+import $ from 'jquery';
 function Perfil(){
 
     const [imagem, setImagem] = useState();
@@ -13,12 +16,16 @@ function Perfil(){
     function cadastrarImagemUsuario(){
         storage.ref(`imagens/${imagem.name}`).put(imagem).then(() => {
             alert("Imagem salva com sucesso!");
+            $('#input-form')[0].reset();
         }).catch(erro => {
             alert(erro);
         })
     }
     return (
         <>
+            {
+                useSelector(state => state.usuarioLogado) === 0 ? <Redirect to='/'/> : null
+            }
             <NavBar />
             <main className="item-container" id="itemPrincipal">
                 <div className="principal">
@@ -29,11 +36,11 @@ function Perfil(){
                         </div>
                         <img src={User} className="img-usuario-perfil img-thumbnail rounded mx-auto d-block" alt="Imagem de usuário"/>
                     </div>
-                    <div className="form-group">
+                    <form id="input-form" className="form-group">
                         <label>Upload de imagem:</label>
                         <input type="file" className="form-control" onChange={(e) => setImagem(e.target.files[0])}/>
                         <button onClick={cadastrarImagemUsuario} type="button" className="btn btn-primary my-2" id="btnEntrar">Fazer upload</button>
-                    </div>
+                    </form>
                     <span><p className="help-text">Personalize seu perfil de usuário</p></span>
                 </div>
             </main>
