375a7614e92f8fe948863eb6186704571a4f7b25
NELSON ANTONIO NETO TONEZE <netoneze@hotmail.com>
2021-08-24 15:33:39 -0300

    Adicionado spinner de carregamento e redirecionamento

--- eaduvida/src/view/newUser/index.js
+++ eaduvida/src/view/newUser/index.js
@@ -3,18 +3,30 @@ import React, {useState} from "react";
 import firebase from '../../config/firebase';
 import 'firebase/auth';
 import $ from "jquery";
+import {useDispatch, useSelector} from "react-redux";
+import {Redirect} from "react-router-dom";
 function NewUser(){
 
     const [email, setEmail] = useState();
     const [senha, setSenha] = useState();
-
+    const [spinner, setSpinner] = useState(false);
+    const dispatch = useDispatch();
     function cadastrar(){
         if ($('#InputUsuario').val() !== '' && $('#InputSenha').val() !== '') {
+            setSpinner(true);
             firebase.auth().createUserWithEmailAndPassword(email, senha)
                 .then(resultado => {
-                    alert("Cadastro realizado!");
+                    firebase.auth().signInWithEmailAndPassword(email, senha).then(() => {
+                        setSpinner(false);
+                        alert("Cadastro realizado com sucesso!")
+                        dispatch({type: 'LOGIN', usuarioEmail: email});
+                    }).catch(erro => {
+                        setSpinner(false);
+                        alert(erro);
+                    })
                 })
                 .catch(erro => {
+                    setSpinner(false);
                     alert(erro);
                 });
         } else {
@@ -23,6 +35,9 @@ function NewUser(){
     }
     return(
         <>
+            {
+                useSelector(state => state.usuarioLogado) > 0 ? <Redirect to="/"/> : null
+            }
             <main className="item-container" id="itemPrincipal">
                 <div className="principal">
                     <h2 className="titulo-principal">Cadastrar</h2>
@@ -37,7 +52,16 @@ function NewUser(){
                             <input onChange={(e) => setSenha(e.target.value)} name="senha" type="password" className="form-control" id="InputSenha"
                                    placeholder="Digite uma senha" required/>
                         </div>
-                        <button onClick={cadastrar} type="button" className="btn btn-primary" id="btnEntrar">Cadastrar</button>
+                        {
+                            spinner ?
+                                <button className="btn btn-primary" type="button" disabled>
+                                    <span className="spinner-border spinner-border-sm" role="status" aria-hidden="true"/>
+                                    &nbsp;Carregando...
+                                </button>
+                                :
+                                <button onClick={cadastrar} type="button" className="btn btn-primary"
+                                        id="btnEntrar">Cadastrar</button>
+                        }
                     </form>
                     <span><p className="help-text">Digite um email e senha para fazer o cadastro</p></span>
                 </div>
